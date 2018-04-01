a2d9bba99712458afb7ff600e0678889b924e21f
laptop_sisi <laptop@sisi.com>
2018-04-01 20:21:47 +0700

    update cron

--- config/cron.js
+++ config/cron.js
@@ -3,7 +3,7 @@ const rp = require('request-promise');
 const maymay = require('../src/salon/maymay.js');
 
 function Job () {
-  cron.schedule('* * * * *', function(){
+  cron.schedule('0 0 * * *', function(){
     maymay();
   });
 
