ccb8897eeac08e625ed7e169df366be2f300603b
komarudin <komarudin@administrators-Mac-mini-4.local>
2018-04-02 16:31:07 +0700

    update

--- src/salon_category/controller.js
+++ src/salon_category/controller.js
@@ -1,5 +1,6 @@
 'use strict';
 const salonCategoryModel = require('./model');
+const category = require('../category/model');
 const promise = require('bluebird');
 const generateToken = require('../../services/token.js');
 const signin = require('../../services/sign-in.js');
@@ -7,13 +8,33 @@ const generateHash = require('../../services/hash.js');
 
 let salon = {
 	list: async function(request, h) {
-		const data = await new promise((resolve, reject) => {
-			salonCategoryModel.find().populate('salon').populate('category').exec((err, result) => {
-				if(!err) {
-					return resolve(result);
-				}
+		let catArr = [];
+		Promise.each = async function(arr, fn) {
+			for(const item of arr) {
+			  const catSal = await fn(item);
+	   
+			  //collect address to db
+			  catArr.push(catSal);
+			}
+		}
+	   
+		function fn(item) {
+		   return new Promise((resolve, reject) => {
+				salonCategoryModel.find({category:item._id}).populate('salon').populate('category').exec((err, result) => {
+					if(!err) {
+						resolve({category:item.name, data:result})
+					}
+				});
+		   });
+		}
 
-				reject(err);
+		const data = await new promise((resolve, reject) => {
+			category.find().exec((err, category) => {
+				if(category.length > 0) {
+					Promise.each(category, fn).then(() =>{
+						resolve(catArr);
+					});
+				}
 			});
 		});
 
