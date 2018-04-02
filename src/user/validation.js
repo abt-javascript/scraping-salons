3d58219620d2ff955155982a2e231bcdd2a65b5a
komarudin <komarudin@administrators-Mac-mini-4.local>
2018-04-02 16:40:40 +0700

    update validation user

--- src/user/validation.js
+++ src/user/validation.js
@@ -3,13 +3,13 @@ const Joi = require('joi');
 const validation = {
   create:{
     fullname: Joi.string().min(3).required(),
-    username: Joi.string().min(5).required(),
-    password: Joi.string().min(8).required(),
+    username: Joi.string().min(3).required(),
+    password: Joi.string().min(3).required(),
     mobile: Joi.string().min(10)
   },
   signin:{
-    username: Joi.string().min(5).required(),
-    password: Joi.string().min(8).required()
+    username: Joi.string().min(3).required(),
+    password: Joi.string().min(3).required()
   }
 };
 
