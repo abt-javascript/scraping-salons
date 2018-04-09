b1f769e8a9b38038578e840498d52cb8898c6fb3
komarudin <komarudin@administrators-Mac-mini-4.local>
2018-04-09 18:37:44 +0700

    update

--- config/cron.js
+++ config/cron.js
@@ -5,7 +5,7 @@ const magdalena = require('../src/salon/magdalena.js');
 const saloncantik = require('../src/salon/saloncantik.js');
 
 function Job () {
-  cron.schedule('* * * * *', function(){
+  cron.schedule('0 0 * * *', function(){
     // maymay().then(() => {
     //   magdalena();
     // });
