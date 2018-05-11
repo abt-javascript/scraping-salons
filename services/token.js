f4090424eb74cfa9608e086111447ea0a7851aa5
komarudin <komarudin@administrators-Mac-mini-4.local>
2018-05-11 10:02:56 +0700

    update expire

--- services/token.js
+++ services/token.js
@@ -4,9 +4,9 @@ const JWT   = require('jsonwebtoken');
 const moment = require('moment');
 
 const token = function (obj) {
-  let plus3Hours = moment().hour(moment().hour() + 3);
+  let plus3Year = moment().year(moment().year() + 3);
   obj['create'] = moment(moment(), "D/M/YYYY  HH:mm:dd").unix();
-  obj['expire'] = moment(plus3Hours, "D/M/YYYY  HH:mm:dd").unix();
+  obj['expire'] = moment(plus3Year, "D/M/YYYY  HH:mm:dd").unix();
 
   const encode =  JWT.sign(obj, `${process.env.SECRET}`);
   
