dc345f5edc93be787888de2110b816a9147e70f7
komarudin <komarudin2211@gmail.com>
2018-06-03 04:41:16 +0700

    update

--- src/user/validation.js
+++ src/user/validation.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 const validation = {
   create:{
     fullname: Joi.string().min(3).required(),
-    username: Joi.string().email(),
+    username: Joi.string().email().required(),
     password: Joi.string().min(3).required(),
     mobile: Joi.string().min(10)
   },
